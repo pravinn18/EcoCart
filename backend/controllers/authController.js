2adf5a560272c5d952225aea696d9393fc8c55e8
Pravin Kumar <pravintch1@gmail.com>
2026-07-09 18:05:55 +0530

    Change Brevo SMTP port

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -32,7 +32,7 @@ const otpStore = new Map();
 const createTransporter = () =>
   nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
+    port: 2525,
     secure: false,
 
     auth: {
